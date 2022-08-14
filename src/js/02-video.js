12c0034c893600f43df10e9532060e365b2ded45
Aleksandor2138 <99901672+Aleksandor2138@users.noreply.github.com>
2022-08-14 19:37:51 +0300

    2 задание

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,25 @@
+import Player from '@vimeo/player';
+let throttle = require('lodash.throttle');
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+const onPlay=(data)=> {
+  // data is an object containing properties specific to that event  
+    localStorage.setItem('videoplayer-current-time', data.seconds);
+    console.log(localStorage.getItem('videoplayer-current-time'));
+};
+player.on(
+    'timeupdate',
+    throttle(onPlay, 1000),
+);
+player.setCurrentTime(localStorage.getItem('videoplayer-current-time'))
+.catch(function (error) {
+    switch (error.name) {
+        case 'RangeError':
+            // the time was less than 0 or greater than the video’s duration
+            break;
 
+        default:
+            // some other error occurred
+            break;
+    }
+});
